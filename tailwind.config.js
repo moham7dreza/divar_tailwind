01c1aa71f12787b659938a90f29993f3b2f2788a
Mohamadreza Rezaei <clash.trial@gmail.com>
2024-01-21 17:19:33 +0330

    add font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                vazirmatn: ["Vazirmatn"]
+            }
+        },
         container: {
             center: true,
         }
